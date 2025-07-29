a143ba7009f8900082781aabaab2cee9f78429dc
MAHALAKSHMI N <mahalakshminandha4@gmail.com>
2025-07-29 10:15:38 +0530

    Removed NOTIFICATION_SERVICE from Git tracking (kept locally)

--- NOTIFICATION_SERVICE/src/main/java/com/tvm/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tvm.repository;
-
-import com.tvm.model.NotificationModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface NotificationRepository extends JpaRepository<NotificationModel, Long> {
-    Optional<NotificationModel> findByType(String type);
-}
