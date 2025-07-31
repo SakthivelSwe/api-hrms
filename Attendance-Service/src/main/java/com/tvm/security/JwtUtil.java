e72539b09fece9dedcb254fa8fb994f628a9832b
Sakthivel <77488929+sakthivelswe@users.noreply.github.com>
2025-07-31 11:38:50 +0530

    HMS - 8 | Attendance Service Implemented

---
+++ Attendance-Service/src/main/java/com/tvm/security/JwtUtil.java
@@ -0,0 +1,41 @@
+package com.tvm.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "BOrzOsp369DFRU6WmvPOwWXk9onIr77I";
+
+    public String extractEmployeeId(String token) {
+        return extractAllClaims(token).getSubject(); // subject = employeeId
+    }
+
+
+    public String extractUsername(String token) {
+        return (String) extractAllClaims(token).get("username");
+    }
+
+    public String extractRole(String token) {
+        Object role = extractAllClaims(token).get("role");
+        return role != null ? role.toString() : null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)   // 0.9.1 API
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
